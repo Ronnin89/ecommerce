cf9b43e54e9dc9c8afb4b1302ee6d424c57b1122
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-03-10 17:17:34 -0300

    testing

--- test/models/cotegory_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CotegoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,22 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @product = Product.new(
+      name: 'limpiaparabrisas',
+      price: 5000,
+      quant: 10
+    )  
+  end
+  
+  test 'product should be valid' do
+    assert @product.valid?
+  end
+
+  test 'product name cant be nil' do
+    @product.name = nil
+    refute @product.valid?, 'no puedes crear un producto sin nombre'
+  end
+
 end
